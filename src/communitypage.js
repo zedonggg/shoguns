f3dcf90ab3a39d4c910f3c80177a3ee1d5e0aeea
Sun <astrolithium@gmail.com>
2024-01-28 22:42:53 +0800

    initial commit

---
+++ src/communitypage.js
@@ -0,0 +1,17 @@
+import NavBar from "./navbar";
+import Footer from "./footer";
+import SearchBar from "./searchbar";
+import CommCards from "./communitycards";
+
+function Community() {
+    return (
+        <>
+        <NavBar />
+        <SearchBar />
+        <CommCards />
+        <Footer />
+        </>
+    )
+}
+
+export default Community;
---
+++ src/facultypage.js
@@ -0,0 +1,17 @@
+import NavBar from "./navbar";
+import Footer from "./footer";
+import SearchBar from "./searchbar";
+import FacCards from "./facultycards";
+
+function Faculty() {
+    return (
+        <>
+        <NavBar />
+        <SearchBar />
+        <FacCards />
+        <Footer />
+        </>
+    )
+}
+
+export default Faculty;
